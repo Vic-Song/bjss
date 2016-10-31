16d244b28fe533215f6c2b9b6a5fed58a0fdc015
Vic-Song <996003243@qq.com>
2016-10-31 08:45:25 +0800

    redirect to previous page after signin and signout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  #configure redirect to previous url after signin ...
+  before_filter :store_current_location, :unless => :devise_controller?
 
-  def after_sign_in_path_for(resource_or_scope)
-  	#stored_location_for(resource_or_scope) || signed_in_root_path(resource_or_scope)
-  	home_index_path
+  # def after_sign_in_path_for(resource_or_scope)
+  # 	home_index_path
+  # end
+private
+
+  def store_current_location
+    store_location_for(:user,request.url)
   end
 
-
   def after_sign_out_path_for(resource_or_scope)
-  	home_index_path
+  	request.referrer || home_index_path
   end
 
 end
