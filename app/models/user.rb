3f695c09e1381346ca0dd5e1ee138e2463a4730d
Vic-Song <996003243@qq.com>
2016-11-06 16:44:10 +0800

    Final alpha version before 1st audit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,22 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+ validates :username,
+ :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  }
+
+ attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+ 	conditions = warden_conditions.dup
+ 	if login = conditions.delete(:login)
+ 		where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value",{:value => login.downcase}]).first
+ 	elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+ 		where(conditions.to_hash).first
+ 	end
+ end
+
 end
