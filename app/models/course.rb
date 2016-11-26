6bd7186736c27515bfa29817f1a7a516fb1a9a6b
Vic-Song <996003243@qq.com>
2016-11-26 10:53:03 +0800

    model modification between course and videos,comments,replies

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,9 @@
 class Course < ActiveRecord::Base
 	has_many :chapters , :dependent => :destroy
+	has_many :videos, through: :chapters
+	has_many :comments, through: :videos
 	has_many :posts , :dependent => :destroy
+	has_many :replies, through: :posts
 	mount_uploader :thumbnail_url,ThumbnailUrlUploader
 	mount_uploader :trailer_url,TrailerUrlUploader
 	validates :title, :teacher, :teacher_intro, :description, :thumbnail_url, :trailer_url, presence: true
